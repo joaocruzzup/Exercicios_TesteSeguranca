19c4bb9021f8d91ea2f86e4fc9c00a05e102b6d9
IrisRPerrorni <iris.perroni@zup.com.br>
2023-06-22 17:58:07 -0300

    :sparkles: feat:  Adicionar metodo criptografar

--- src/Seguranca/MainCriptografia.java
+++ src/Seguranca/MainCriptografia.java
@@ -1,7 +1,15 @@
 package Seguranca;
 
 public class MainCriptografia {
+
     public static void main(String[] args) {
 
+    }
+    public static String criptografar(String mensagem, int chave) {
+        String Criptmensagem = "";
+        for(int i = 0; i < mensagem.length(); i++) {
+            Criptmensagem += (char) (mensagem.charAt(i) + chave);
+        }
+        return Criptmensagem;
     }
 }
